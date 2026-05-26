2c19fc1ad990a42207f61d46a2f3df2c66d9d02e
huy2342 <huybluyt@gmail.com>
2026-05-26 13:45:59 +0700

    Delete buoi1 directory

--- buoi1/1/cau10.py
+++
@@ -1,9 +0,0 @@
- #Bài 10.Vị trí xuất hiện cuối cùng
-def vi_tri_cuoi(a, x):
-    for i in range(len(a) - 1, -1, -1):
-        if a[i] == x:
-            return i
-    return -1
-a =[4, 1, 4, 9, 4]
-x =int(input("Nhap x: "))
-print("Vi tri cuoi:", vi_tri_cuoi(a, x))
